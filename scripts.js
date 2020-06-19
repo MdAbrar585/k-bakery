00239b4cf315ed80a8087affc72cf48194333948
MdAbrar585 <abrarm585@gmail.com>
2020-06-19 19:56:54 +0600

    navbar fixed

--- scripts.js
+++ scripts.js
@@ -5,6 +5,12 @@ $(function(){
         $('.nav-list').toggleClass('open');
     });
 
+    $("a.nav-link").on("click",function(){
+        $('.toggle').toggleClass('open');
+        $('.nav-list').toggleClass('open');
+
+//or simply $("#navbar").addClass("makeDisappear")
+})
     AOS.init({
         easing: 'ease',
         duration: 1000,
